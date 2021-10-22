7526d539c61e7445bc8fdd11611e541a8952d269
jcptorres <jcptorress@gmail.com>
2021-10-21 20:02:58 -0500

    added Count & Index methods

---
+++ 06-metodos/metodos-de-lista.py
@@ -0,0 +1,33 @@
+# Metodos de listas
+
+# append
+
+a_list = [1,2,3,4,5]
+a_list.append(6)
+print(a_list)
+
+# clear
+
+a_list.clear()
+print(a_list)
+
+# extend
+# El metodo extend() une lista con otra
+
+l1= [1,2,3,4]
+l2= [8,4,6,9]
+print (l1)
+
+l1.extend(l2)
+print (l1)
+
+# Count
+
+a = ['juan','fernando', 'Juan', 'carlos', 'juan']
+print (a.count('juan'))
+
+# index
+
+print (a.index('carlos'))
+
+
